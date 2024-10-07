5d1f0a76ee6d3ed8744fb4ae637839f28faed9ac
Sabin <sawin0>
2024-10-07 13:27:16 +0545

    chore: test code cleanup on example

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -6,10 +6,8 @@
 // For more information about Flutter integration tests, please see
 // https://docs.flutter.dev/cookbook/testing/integration/introduction
 
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
 import 'package:social_share_plus/social_share_plus.dart';
 
 void main() {
@@ -17,9 +15,5 @@ void main() {
 
   testWidgets('getPlatformVersion test', (WidgetTester tester) async {
     final SocialSharePlus plugin = SocialSharePlus();
-    final String? version = await plugin.getPlatformVersion();
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
-    expect(version?.isNotEmpty, true);
   });
 }
